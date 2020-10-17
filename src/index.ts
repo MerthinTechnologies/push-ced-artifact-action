45bd29497416e58b56b12bc80f464674635ffba1
Joaquin Pinillos <jpinilloslr@gmail.com>
2020-10-17 16:21:57 +0200

    Change  unhandledRejection rejections.

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,47 @@
 import * as core from '@actions/core';
 import { PushCommandHandler } from '@ced/cli';
 
-const run = async function() {
-  try {
-    const cliToken = core.getInput('cli-token') || process.env['CED_CLI_TOKEN'];
-    const environment = core.getInput('environment') || process.env['CED_ENVIRONMENT'];
-    const path = core.getInput('path') || process.env['CED_PROJECT_PATH'];
-    const version = core.getInput('version');
-    const pushAsDraft = !!core.getInput('push-as-draft') 
-      ? core.getInput('push-as-draft').toLowerCase() !== 'false'
-      : false;
+const run = async function () {
+  process.on('uncaughtException', (err) => {
+    console.log(err);
+    core.setFailed(err.message);
+    process.exit(1);
+  });
 
-    if (!cliToken) {
-      throw new Error(`Missing CED CLI token. Provide a CLI token by "cli-token" input parameter or define a variable "CED_CLI_TOKEN".`)
-    }
+  process.on('unhandledRejection', (err) => {
+    core.setFailed('Unhandled rejection');
+    process.exit(1);
+  });
 
-    if (environment) {
-      console.log(`Using environment: ${environment}`);
-    }
+  const cliToken = core.getInput('cli-token') || process.env['CED_CLI_TOKEN'];
+  const environment =
+    core.getInput('environment') || process.env['CED_ENVIRONMENT'];
+  const path = core.getInput('path') || process.env['CED_PROJECT_PATH'];
+  const version = core.getInput('version');
+  const pushAsDraft = !!core.getInput('push-as-draft')
+    ? core.getInput('push-as-draft').toLowerCase() !== 'false'
+    : false;
 
-    if (path) {
-      process.chdir(path);
-      console.log(`Using ${path} as working directory`);
-    }
-
-    const command = new PushCommandHandler(cliToken);
-    await command.run(environment, version, pushAsDraft);
-  } catch (error) {
-    console.log(error);
-    core.setFailed(error.message);
+  if (!cliToken) {
+    throw new Error(
+      `Missing CED CLI token. Provide a CLI token by "cli-token" input parameter or define a variable "CED_CLI_TOKEN".`
+    );
   }
-}
 
-run();
+  if (environment) {
+    console.log(`Using environment: ${environment}`);
+  }
+
+  if (path) {
+    process.chdir(path);
+    console.log(`Using ${path} as working directory`);
+  }
+
+  const command = new PushCommandHandler(cliToken);
+  await command.run(environment, version, pushAsDraft);
+};
+
+run().catch((error) => {
+  console.log(error);
+  core.setFailed(error.message);
+});
